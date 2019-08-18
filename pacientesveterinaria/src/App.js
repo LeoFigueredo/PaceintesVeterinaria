f34b33089cee562ed2e3536ef7958d32b9e8e147
Leonardo Figueredo <Leonardo.Figueredo@techandsolve.com>
2019-08-18 17:28:45 -0500

    Eliminando cita del state

--- pacientesveterinaria/src/App.js
+++ pacientesveterinaria/src/App.js
@@ -20,6 +20,18 @@ class App extends Component {
 
 
   }
+
+  eliminarCita = id => {
+    //tomar una copia del state
+    const citasActuales = [...this.state.citas];
+    
+    //utilizar filter para sacar el elemento @id del arreglo
+    const citas = citasActuales.filter(cita => cita.id !== id )
+    //actualizar el state 
+    this.setState({
+      citas
+    })
+  }
   render() {
     return ( 
       <div className="Container">
@@ -35,6 +47,7 @@ class App extends Component {
               <div className="mt-5 col-md-10 mx-auto">
                 <ListaCitas
                   citas={this.state.citas}
+                  eliminarCita={this.eliminarCita}
                 />
               </div>
           </div>
